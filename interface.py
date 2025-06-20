83704847de431044c94d3019e06301fa3b48612d
Abhishek Kumar <139259158+Abhishek-0077@users.noreply.github.com>
2025-06-20 12:16:53 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,24 +1,20 @@
-from model_loader import ModelLoader
-from chat_memory import ChatMemory   
-
-
 def main():
-    #model_name = "openchat/openchat-3.5-0106"
-    model_loader = ModelLoader(model_name="microsoft/phi-2")
-    memory = ChatMemory(max_turns=5)
-    
     print("Chatbot started. Type /exit to quit.")
     while True:
         user_input = input("You: ").strip()
         if user_input.lower() == "/exit":
-            print("Goodbye!")
+            print("Exiting chatbot. Goodbye!")
             break
+        elif user_input.lower()=="history":
+            print(memory.history())
+            continue
         
         memory.add_user_message(user_input)
         prompt = memory.get_prompt()
         reply = model_loader.generate(prompt)
+      
         print("Assistant:", reply)
         memory.add_assistant_message(reply)
-
+ 
 if __name__ == "__main__":
     main()
